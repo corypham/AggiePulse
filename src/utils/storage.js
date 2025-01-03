089d9a9f6646fc29aa85780a542183f24498069c
corypham <corypham1@gmail.com>
2025-01-02 17:54:53 -0800

    🏗️  Directory restructuring and changed app entry to index and layout script files

--- src/utils/storage.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const cacheData = async (key, data) => {
-  try {
-    await AsyncStorage.setItem(key, JSON.stringify(data));
-  } catch (error) {
-    console.error('Error caching data:', error);
-  }
-}; 
