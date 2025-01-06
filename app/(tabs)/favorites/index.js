66763d02c2ee7624bf4ba410258dd37c2d680c57
corypham <corypham1@gmail.com>
2025-01-06 01:08:41 -0800

    🍕 Component changes

--- app/(tabs)/favorites/index.js
+++ app/(tabs)/favorites/index.js
@@ -5,6 +5,15 @@ const index = () => {
   return (
     <SafeAreaView className="flex-1  bg-gray-800">
       <Text className="text-white text-3xl font-bold m-4">This is the Favorites tab</Text>
+      <View className="flex-1 bg-white">
+        <Text className="text-black text-3xl font-bold m-4">This is the Favorites tab</Text>
+      </View>
+      <View className="flex-1 bg-gray-600">
+        <Text className="text-white text-3xl font-bold m-4">This is the Favorites tab</Text>
+      </View>
+      <View className="flex-1 bg-orange-500">
+        <Text className="text-white text-3xl font-bold m-4">This is the Favorites tab</Text>
+      </View>
     </SafeAreaView>
   );
 };
