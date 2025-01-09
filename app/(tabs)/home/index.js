b119c8800faeb2181fe89fd02c35c0a2e8465569
corypham <corypham1@gmail.com>
2025-01-09 03:53:24 -0800

    🪅 Fixed home and fave page

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -8,12 +8,18 @@ import Settings from "./Settings";
 const index = () => {
   const router = useRouter();
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-gray-800">
-      <Text className="text-white text-3xl font-bold m-4">This is the Home tab that were working on</Text>
+    <SafeAreaView className="flex-1 items-start justify-center bg-gray-800">
+      <Text className="text-white text-3xl font-bold m-1">This is the Home tab that were working on</Text>
       <Button title="About" onPress={() => router.push("home/About")} />
       <Button title="Settings" onPress={() => router.push("home/Settings")} />
       <View className="flex-1 flex-row flex-wrap p-2">
         <ScrollView>
+          <Card title="Hello 1" description="This is a card" />
+          <Card title="Card 2" description="This is a card" />
+          <Card title="Card 3" description="This is a card" />
+          <Card title="Card 4" description="This is a card" />
+          <Card title="Card 5" description="This is a card" />
+          <Card title="Card 6" description="This is a card" />
           <Card title="Hello 1" description="This is a card" />
           <Card title="Card 2" description="This is a card" />
           <Card title="Card 3" description="This is a card" />
