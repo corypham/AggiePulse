d2d0251e7407bc1da0f3f6edc1d8a2cd80e0b2b7
corypham <corypham1@gmail.com>
2025-01-27 04:44:38 -0800

    🏡 Fixed home page

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,21 +1,12 @@
-import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
-import { useRouter } from "expo-router";
 
 export default function Header({ title = 'Default Title' }) { 
-  const router = useRouter();
-
   return (
-  <SafeAreaView className="bg-white">
-    <View className="flex-row justify-between items-center bg-white px-6 py-2">
-      <TouchableOpacity 
-        onPress={() => router.back()}
-        className="flex-row items-center"
-      >
-        <Text className="text-blue-700 text-lg font-bold">← Back</Text>
-      </TouchableOpacity>
-      <Text className="text-black text-lg font-bold">{title}</Text>
-    </View>
-  </SafeAreaView>
+    <SafeAreaView className="bg-transparent">
+      <View className="flex-row justify-center items-center px-6 py-2">
+        <Text className="text-black text-lg font-bold">{title}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
