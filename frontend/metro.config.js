3be99c9fe37d84b368fac20acc0d2cc3dff09987
corypham <corypham1@gmail.com>
2025-01-27 04:45:36 -0800

    ✨ Configured presets

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -4,6 +4,16 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+config.resolver = {
+  ...config.resolver,
+  assetExts: [...config.resolver.assetExts.filter((ext) => ext !== "svg")],
+  sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
 // Add the root directory as a watch folder
 config.watchFolders = [
   path.resolve(__dirname, "../"), // Monorepo root
