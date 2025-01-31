a834c3000c7e488f38f9f6b857ad02da603bc46b
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2025-01-30 23:20:52 -0800

    added first filter toggle button

--- frontend/app/screens/FilterPage.tsx
+++ frontend/app/screens/FilterPage.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import FilterToggleButton from '../components/FilterToggleButton';
 
 const FilterPage: React.FC = () => {
-    return (
-        <div>
-        Filter Page
-        </div>
-    );
+  const [isNotBusyActive, setIsNotBusyActive] = useState(false);
+
+  const handleNotBusyPress = () => {
+    setIsNotBusyActive(!isNotBusyActive);
+  };
+
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Text style={{ fontFamily: 'Aileron-Bold', marginLeft: 4, fontSize: 18, marginTop: 20 }}>
+        Location Type
+      </Text>
+      <FilterToggleButton
+        label="Study Spaces"
+        isActive={isNotBusyActive}
+        onPress={handleNotBusyPress}
+      />
+      {/* Add more FilterToggleButton components as needed */}
+    </View>
+  );
 };
 
 export default FilterPage;
