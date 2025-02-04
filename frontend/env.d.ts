ebbb430990db5434aef816931d565211a04d5726
corypham <corypham1@gmail.com>
2025-02-03 17:00:42 -0800

    🔐 Configured API keys

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,5 +1,6 @@
 declare module '@env' {
   export const GOOGLE_MAPS_API_KEY_IOS: string;
   export const GOOGLE_MAPS_API_KEY_ANDROID: string;
-  export const GOOGLE_MAPS_STYLE_ID: string;
+  export const GOOGLE_MAPS_STYLE_ID_IOS: string;
+  export const GOOGLE_MAPS_STYLE_ID_ANDROID: string;
 }
